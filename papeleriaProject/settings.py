bc96fc000653144336319d035f0753a7b06cf1f9
Rodal 95 <alday559@gmail.com>
2023-11-19 12:14:28 -0300

    ultima mejora

--- papeleriaProject/settings.py
+++ papeleriaProject/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['52.90.101.20','localhost','127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
